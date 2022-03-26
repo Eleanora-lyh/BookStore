d81373b344d2d8a2e92bbcb4b10f7ef986b29294
Eleanora <Eleanora@qq.com>
2022-03-26 20:37:51 +0800

    利用session解决表单重复提交问题，添加了实时验证码的刷新功能

--- src/com/lyh/web/UserServlet.java
+++ src/com/lyh/web/UserServlet.java
@@ -13,11 +13,14 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import static com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY;
+
 public class UserServlet extends BaseServlet {
     UserService userService = new UserServiceImpl();
+
     protected void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //(1)获取请求的参数
-        User user = WebUtils.copyParamToBean(req.getParameterMap(),new User());
+        User user = WebUtils.copyParamToBean(req.getParameterMap(), new User());
         //(2)判断用户名密码是否正确
         User loginUser = userService.login(user);
         if (loginUser == null) {
@@ -27,16 +30,30 @@ public class UserServlet extends BaseServlet {
             req.getRequestDispatcher("/pages/user/login.jsp").forward(req, resp);
         } else {
             //正确，进入登录成功界面
+            // 保存用户登录的信息到Session域中
+            req.getSession().setAttribute("user", loginUser);
             req.getRequestDispatcher("/pages/user/login_success.jsp").forward(req, resp);
         }
     }
+
+    protected void logout(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //(1)销毁session的信息
+        req.getSession().invalidate();
+        //(2)重定向到首页
+        resp.sendRedirect(req.getContextPath());
+    }
+
     protected void regist(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //1、获取请求的参数
         User user = WebUtils.copyParamToBean(req.getParameterMap(), new User());
-//        System.out.println(user.getUsername() + " " +user.getPassword()+ " " + user.getEmail());
+        // 获取Session中的验证码 token
+        String token = (String) req.getSession().getAttribute(KAPTCHA_SESSION_KEY);
+        // 删除 Session中的验证码
+        req.getSession().removeAttribute(KAPTCHA_SESSION_KEY);
+        //用户传进来的验证码 code
         String code = req.getParameter("code");
-        //2、检查 验证码是否正确  === 写死,要求验证码为:abcde
-        if ("abcde".equalsIgnoreCase(code)) {
+        //2、检查 验证码是否正确 token==code
+        if (token!=null && token.equalsIgnoreCase(code)) {
             //3、检查 用户名是否可用
             if (userService.existsUsername(user.getUsername())) {//已存在，不可用
                 req.setAttribute("username", user.getUsername());
--- web/pages/user/regist.jsp
+++ web/pages/user/regist.jsp
@@ -15,6 +15,13 @@
     </style>
     <script type="text/javascript">
         $(function () { //页面加载完成后
+            // 给验证码的图片，绑定单击事件
+            $("#code_img").click(function () {
+                // 在事件响应的function函数中有一个this对象。这个this对象，是当前正在响应事件的dom对象
+                // src属性表示验证码img标签的 图片路径。它可读，可写
+                // alert(this.src);
+                this.src = "${basePath}kaptcha.jpg?d=" + new Date();
+            });
             //给注册绑定单击事件
             $("#sub_btn").click(function () {
                 //验证用户名： 必须由字母， 数字下划线组成， 并且长度为 5 到 12 位
@@ -121,8 +128,8 @@
                         <br/>
                         <br/>
                         <label>验证码：</label>
-                        <input class="itxt" type="text" style="width: 150px;" name="code" id="code"/>
-                        <img alt="" src="static/img/code.bmp" style="float: right; margin-right: 40px">
+                        <input class="itxt" type="text" name="code" style="width: 80px;" id="code" />
+                        <img id="code_img" alt="" src="kaptcha.jpg" style="float: right; margin-right: 40px; width: 110px; height: 30px;">
                         <br/>
                         <br/>
                         <input type="submit" value="注册" id="sub_btn"/>
